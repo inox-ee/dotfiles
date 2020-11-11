cba02dc0eec4c4f2deeb998954cbaf7ed0e8046c
Kazuma Inokuchi <ddz92478@gmail.com>
2020-11-11 12:08:18 +0900

    install nvm&yarn

--- .zsh.d/20-application.zsh
+++ .zsh.d/20-application.zsh
@@ -5,3 +5,11 @@
 
 # rbenv
 eval "$(rbenv init -)"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# yarn
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
