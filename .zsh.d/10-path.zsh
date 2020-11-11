110f4373ee9dff6b231ad7212b90f4644813b081
Kazuma Inokuchi <ddz92478@gmail.com>
2020-11-11 12:07:07 +0900

    update path&alias

--- .zsh.d/10-path.zsh
+++ .zsh.d/10-path.zsh
@@ -1,3 +1,4 @@
 # PATH
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$PATH:/mnt/c/Program Files/Docker/Docker/resources/bin" # https://github.com/docker/compose/issues/7495
