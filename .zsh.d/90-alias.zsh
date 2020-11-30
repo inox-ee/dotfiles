f2fc6eca665b410bc7985a493180f6d0ea56996f
Kazuma Inokuchi <ddz92478@gmail.com>
2020-11-30 12:01:45 +0900

    create functions

--- .zsh.d/90-alias.zsh
+++ .zsh.d/90-alias.zsh
@@ -7,7 +7,7 @@ alias python='python3'
 alias rm='echo "This is not the command you are looking for."; false'
 alias ln='ln -iv'
 alias ventus='cd ~/Ventus/Orical_api && sudo service redis-server start && docker start mysql-server && docker-compose -f ~/.docker/elasticsearch/docker-compose.yml up -d && docker ps -a'
-alias ventus-stop='cd ~/Ventus/Orical_api && sudo service redis-server stop && docker stop mysql-server && docker-compose -f ~/.docker/elasticsearch/docker-compose.yml stop && docker ps -a'
+alias ventus-stop='cd ~/Ventus/Orical_api && sudo service redis-server stop && docker stop mysql-server && docker-compose -f ~/.docker/elasticsearch/docker-compose.yml stop && docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}"'
 alias adb='/mnt/c/Users/ddz92/AppData/Local/Android/Sdk/platform-tools/adb.exe'
 alias vpn-elab="ssh -D 10080 user@elab-gw.ic.i.u-tokyo.ac.jp"
 
---
+++ .zsh.d/91-function.zsh
@@ -0,0 +1,32 @@
+autoload -Uz catch
+autoload -Uz throw
+
+function gittemplate() {
+  if [ $# -ne 2 ]; then
+    echo "Specify a source Git repogitory and new project name"
+    return
+  fi
+
+  {
+    git clone --depth 1 $1 $2 || throw 'NetworkException'
+    cd $2 || throw 'NotFoundException'
+    echo "current directory: $(pwd) ok?(y/N): "; read -q || throw 'InvalidError'
+    echo ""
+    trash ./.git
+    git init
+    git add .
+    git commit -m "Initial Commit"
+    git log --oneline
+    echo "done"
+  } always {
+    if catch '*'; then
+      case $CAUGHT in
+        (*)
+          echo "error"
+          ;;
+      esac
+    fi
+  }
+  return
+}
+
