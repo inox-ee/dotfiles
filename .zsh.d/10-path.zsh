5c0d792505a7a25b2e5c0075305cc2ed077e677d
Kazuma Inokuchi <ddz92478@gmail.com>
2020-12-28 10:05:35 +0900

    add path

--- .zsh.d/10-path.zsh
+++ .zsh.d/10-path.zsh
@@ -2,3 +2,5 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$PATH:/mnt/c/Program Files/Docker/Docker/resources/bin" # https://github.com/docker/compose/issues/7495
+export PATH="$HOME/protoc3/bin:$PATH"
+
