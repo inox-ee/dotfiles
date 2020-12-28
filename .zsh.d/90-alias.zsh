082f6d9d721b95b267109c975c2669b1cdf4ea15
Kazuma Inokuchi <ddz92478@gmail.com>
2020-12-28 10:05:51 +0900

    add alias

--- .zsh.d/90-alias.zsh
+++ .zsh.d/90-alias.zsh
@@ -10,4 +10,5 @@ alias ln='ln -iv'
 # alias ventus-stop='cd ~/Ventus/Orical_api && sudo service redis-server stop && docker stop mysql-server && docker-compose -f ./docker/elasticsearch/docker-compose.yml down -v && docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}"'
 alias adb='/mnt/c/Users/ddz92/AppData/Local/Android/Sdk/platform-tools/adb.exe'
 alias vpn-elab="ssh -D 10080 user@elab-gw.ic.i.u-tokyo.ac.jp"
+alias dc='docker-compose'
 
