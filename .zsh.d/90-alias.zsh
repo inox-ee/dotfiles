c63ceda068eb677cde7b2db1dd49278d00e6a7df
Kazuma Inokuchi <ddz92478@gmail.com>
2021-01-13 20:08:32 +0900

    update alias

--- .zsh.d/90-alias.zsh
+++ .zsh.d/90-alias.zsh
@@ -6,9 +6,8 @@ alias berspec='bundle exec rspec'
 alias python='python3'
 alias rm='echo "This is not the command you are looking for."; false'
 alias ln='ln -iv'
-# alias ventus='cd ~/Ventus/Orical_api && sudo service redis-server start && docker start mysql-server && docker-compose -f ./docker/elasticsearch/docker-compose.yml up -d && docker ps -a'
-# alias ventus-stop='cd ~/Ventus/Orical_api && sudo service redis-server stop && docker stop mysql-server && docker-compose -f ./docker/elasticsearch/docker-compose.yml down -v && docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}"'
 alias adb='/mnt/c/Users/ddz92/AppData/Local/Android/Sdk/platform-tools/adb.exe'
 alias vpn-elab="ssh -D 10080 user@elab-gw.ic.i.u-tokyo.ac.jp"
 alias dc='docker-compose'
+alias cl='clear'
 
