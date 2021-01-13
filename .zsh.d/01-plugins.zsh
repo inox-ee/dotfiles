885d8f06623378118eae370a331de835acb71476
Kazuma Inokuchi <ddz92478@gmail.com>
2021-01-14 00:15:12 +0900

    各インストールコマンドに quiet / silent オプションを追加 (#4)

--- .zsh.d/01-plugins.zsh
+++ .zsh.d/01-plugins.zsh
@@ -1,15 +1,24 @@
 # My Zsh plugin
 
 ## Zsh-Users
+zinit ice silent
 zinit load zsh-users/zsh-syntax-highlighting
+zinit ice silent
+zinit load zsh-users/zsh-syntax-highlighting
+zinit ice silent
 zinit load zsh-users/zsh-completions
+zinit ice silent
 zinit load zsh-users/zsh-autosuggestions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
 
 ## Prezto
+zinit ice silent
 zinit snippet PZT::modules/helper/init.zsh
+zinit ice silent
 zinit snippet PZT::modules/utility/init.zsh
+zinit ice silent
 zinit snippet PZT::modules/directory/init.zsh
+zinit ice silent
 zinit snippet PZT::modules/completion/init.zsh
 
 # others
--- bin/init/01-install.zsh
+++ bin/init/01-install.zsh
@@ -5,7 +5,7 @@ echo -n "Do you want to run $0?(y/N): "; read -q && echo "" || exit 0
 # =====
 # install essentials
 # =====
-(sudo apt install -y build-essential libbz2-dev libdb-dev \
+(sudo apt install -y -qq build-essential libbz2-dev libdb-dev \
   libreadline-dev libffi-dev libgdbm-dev liblzma-dev \
   libncursesw5-dev libsqlite3-dev libssl-dev \
   zlib1g-dev uuid-dev tk-dev \
@@ -21,7 +21,7 @@ echo -n "Do you want to run $0?(y/N): "; read -q && echo "" || exit 0
 # =====
 echo -n "$(python3 --version), is it ok?(y/N): "
 if read -q && echo ""; then
-  curl https://bootstrap.pypa.io/get-pip.py | python3
+  curl -s https://bootstrap.pypa.io/get-pip.py | python3
   echo $(pip --version)
 else
   echo "\e[31m[CAUTION!] please install Python 3.0.0>\e[m"
@@ -31,7 +31,7 @@ fi
 # install fzf
 # =====
 if ! [ -d $HOME/.fzf ]; then
-  git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+  git clone -q --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
   echo "\e[33m[WARNING] please enable fzf-keybinding and disable other settings.\e[m"
   $HOME/.fzf/install
   rm $HOME/.fzf.bash
@@ -42,28 +42,28 @@ fi
 # install vim-hybrid
 # =====
 mkdir -p ~/.vim/colors
-(cd $ZDOTDIR/.vim/colors && curl -O https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim)
+(cd $ZDOTDIR/.vim/colors && curl -s -O https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim)
 
 # =====
 # install rbenv
 # =====
 if ! whence -w rbenv; then
-  git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
-  git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+  git clone -q https://github.com/rbenv/rbenv.git $HOME/.rbenv
+  git clone -q https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 fi
 
 # =====
 # install trash-cli
 # =====
-whence -w pip && whence -w trash || pip install trash-cli
+whence -w pip && whence -w trash || pip install -q trash-cli
 
 # =====
 # install tree
 # =====
-whence -w tree || sudo apt install tree
+whence -w tree || sudo apt install -y -qq tree
 
 # =====
 # install expect (for `unbuffer` command)
 # =====
-whence -w unbuffer || sudo apt install expect
+whence -w unbuffer || sudo apt install -y -qq expect
 
