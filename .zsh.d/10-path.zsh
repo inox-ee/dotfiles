3805e7aeb807a6929ebd0f4df002c110a53aff31
Kazuma Inokuchi <ddz92478@gmail.com>
2021-01-21 18:19:52 +0900

    add gopath

--- .zsh.d/10-path.zsh
+++ .zsh.d/10-path.zsh
@@ -3,4 +3,5 @@ export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$PATH:/mnt/c/Program Files/Docker/Docker/resources/bin" # https://github.com/docker/compose/issues/7495
 export PATH="$HOME/protoc3/bin:$PATH"
+export PATH="$PATH:/usr/local/go/bin"
 
