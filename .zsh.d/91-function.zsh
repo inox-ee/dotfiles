8b98b1c438dcefc4a9efeedf23169ce5cad00721
Kazuma Inokuchi <ddz92478@gmail.com>
2021-02-18 15:13:53 +0900

    add leetgode()

--- .zsh.d/91-function.zsh
+++ .zsh.d/91-function.zsh
@@ -38,3 +38,35 @@ function treeex() {
   tree -aFC -L 2
 }
 
+function leetgode() {
+  if [ $# -ne 2 ]; then
+    echo "指定された引数は $# 個です。" 1>&2
+    echo "実行するには問題番号とタイトルを指定してください。" 1>&2
+    return
+  fi
+
+  CURRENTPATH=$(pwd)
+
+  if [ ${CURRENTPATH##*/} != leetgode ]; then
+    echo "現在のディレクトリは ${CURRENTPATH} です。" 1>&2
+    echo "実行するには path/to/leetgode に移動してください。" 1>&2
+    return
+  fi
+
+  {
+    mkdir -p "./leetcode/$1.$2"
+    cd $_
+    touch "$2.go"
+    touch "$2_test.go"
+    echo "create $1.$2/$2.go, $2_test.go"
+  } always {
+    if catch '*'; then
+      case $CAUGHT in
+        (*)
+          echo "error"
+          ;;
+      esac
+    fi
+  }
+  return
+}
