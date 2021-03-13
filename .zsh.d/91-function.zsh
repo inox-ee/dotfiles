f3ba9f7e6931227d40a8dadc181f99667c0171ae
Kazuma Inokuchi <ddz92478@gmail.com>
2021-03-13 11:26:33 +0900

    fix gittemplate

--- .zsh.d/91-function.zsh
+++ .zsh.d/91-function.zsh
@@ -2,13 +2,24 @@ autoload -Uz catch
 autoload -Uz throw
 
 function gittemplate() {
+  local -A opthash
+  zparseopts -D -A opthash -- -branch:
+
+  if [[ -n "${opthash[(i)--branch]}" ]]; then
+    echo "-s option : ${opthash[--branch]}"
+  fi
+
   if [ $# -ne 2 ]; then
     echo "Specify a source Git repogitory and new project name"
     return
   fi
 
   {
-    git clone --depth 1 $1 $2 || throw 'NetworkException'
+    if [[ -n "${opthash[(i)--branch]}" ]]; then
+      git clone -b ${opthash[--branch]} --depth 1 $1 $2 || throw 'NetworkException'
+    else
+      git clone --depth 1 $1 $2 || throw 'NetworkException'
+    fi
     cd $2 || throw 'NotFoundException'
     echo "current directory: $(pwd) ok?(y/N): "; read -q || throw 'InvalidError'
     echo ""
@@ -54,16 +65,20 @@ function leetgode() {
   fi
 
   {
-    mkdir -p "./leetcode/$1.$2"
-    cd $_
-    touch "$2.go" && touch "README.md"
-    curl -LO "https://raw.githubusercontent.com/aQuaYi/LeetCode-in-Go/master/Algorithms/$1.$2/$2_test.go"
-    echo "create $1.$2/$2.go, $2_test.go"
+    local response=$(curl -LOfsS -w '%{http_code}\n' "https://raw.githubusercontent.com/aQuaYi/LeetCode-in-Go/master/Algorithms/$1.$2/$2_test.go")
+    if [ $response -eq 200 ]; then
+      mkdir -p "./leetcode/$1.$2"
+      mv ./$2_test.go $_/
+      cd $_
+      touch "$2.go" && touch "README.md" && echo "create $1.$2/$2.go, $2_test.go"
+    else
+      throw "HTTP Error: $response"
+    fi
   } always {
     if catch '*'; then
       case $CAUGHT in
         (*)
-          echo "error"
+          echo $CAUGHT
           ;;
       esac
     fi
