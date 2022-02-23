8c00d573db3d0a0d1a05b9e3d2af017aa599a07a
Kazuma Inokuchi <ddz92478@gmail.com>
2022-02-23 12:50:54 +0900

    update alias

--- .zsh.d/90-alias.zsh
+++ .zsh.d/90-alias.zsh
@@ -11,6 +11,7 @@ alias cl='clear'
 ## Ruby
 alias ber='bundle exec rails'
 alias berspec='bundle exec rspec'
+alias update-rbenv='cd ~/.rbenv && git pull && cd ~/.rbenv/plugins/ruby-build && git pull'
 
 ## Python
 alias python='python3'
@@ -28,5 +29,5 @@ alias grsa='git restore .'
 
 # Marp <https://github.com/marp-team/marp-cli>
 alias marp-npx='npx @marp-team/marp-cli@latest'
-alias marp-docker='docker run --rm -v "$(pwd):/home/marp/app" -e MARP_USER=$(id -u):$(id -g) -e LANG="ja_JP.UTF-8" marpteam/marp-cli --allow-local-files'
+alias marp-docker='docker run -it --name=marp-docker --rm -v "$(pwd):/home/marp/app" -e MARP_USER=$(id -u):$(id -g) -e LANG="ja_JP.UTF-8" marpteam/marp-cli --allow-local-files'
 
